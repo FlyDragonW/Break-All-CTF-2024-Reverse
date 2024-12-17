6b73f137439ac90730522af238733b0f94d0578a
Flydragon <96608738+FlyDragonW@users.noreply.github.com>
2024-12-17 08:54:26 +0800

    Update main.c

--- Welcome/main.c
+++ Welcome/main.c
@@ -13,19 +13,19 @@ void flag(){
 int main() {
     srand(time(NULL));
     printf("\033[31mWelcome to Break-All-CTF-Reverse!\n");
-    printf("\033[36mIn this CTF game, you need to solve challenge with some basic Reverse skill.\n");
+    printf("\033[36mIn this CTF game, you need to solve challenges with some basic Reverse skill.\n");
     printf("I hope that you will learn something after this game :)\n");
     printf("Flag format: flag{.*}\n");
     printf("\n");
-    printf("\033[31mBelow is the categories of the challenges:\n");
-    printf("\033[36mNote that first challenge in each categories should be easy and could be solved without prior knowledge.\n");
-    printf("If you Break ALL every single challenge, feel free to contact me for a special prize! (only the first one)\n");
+    printf("\033[31mBelow are the categories of the challenges:\n");
+    printf("\033[36mNote that the first challenge in each categories supposed to be very easy and can be solved without prior knowledge, highly recommend to check out every category!\n");
+    printf("If you Break ALL challenge, feel free to contact me for a special prize! (only the first one)\n");
     printf("\n");
     printf("\033[32mELF\n");
-    printf("\033[36mQuite striaght forward, reverse ELF file.\n");
+    printf("\033[36mQuite straight forward, reverse ELF file.\n");
     printf("\n");
     printf("\033[32mUnity Game Hacking\n");
-    printf("\033[36mGenaral game hacking skill and something specific for unity games.\n");
+    printf("\033[36mGeneral game hacking skill and something specific for unity games.\n");
     printf("\n");
     printf("\033[32mAndroid\n");
     printf("\033[36mFind out what a APK file does.\n");
@@ -36,7 +36,7 @@ int main() {
     printf("\033[32mPacker\n");
     printf("\033[36mMeet common packer and custom packer.\n");
     printf("\n");
-    printf("\033[31mThat's it!, Good Luck and Happy Reversing!\n");
+    printf("\033[31mThat's it! Good Luck and Happy Reversing!\n");
     printf("\n");
     printf("\033[0mHere is your flag: ");
     flag();
